cc2a497095f8c91de294521a3f9314732597f493
Patrick Porto <patrickporto.dev@gmail.com>
2021-10-07 14:17:48 -0300

    Alteração tela iniciaç

--- lib/mainScreen/main_screen.dart
+++ lib/mainScreen/main_screen.dart
@@ -6,10 +6,12 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Container(
+        color: Colors.blue[600],
         child: Padding(
           padding: const EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 24.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Text("data"),
             ],
